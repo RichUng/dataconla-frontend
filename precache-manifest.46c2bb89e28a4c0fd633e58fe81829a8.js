4764938db0797274044f8955a02067e049faf14d
Rich Ung (via Travis CI) <ricky196@yahoo.com>
2019-10-20 00:26:51 +0000

    Deploy RichUng/dataconla-frontend to github.com/RichUng/dataconla-frontend.git:gh-pages

---
+++ precache-manifest.46c2bb89e28a4c0fd633e58fe81829a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d88913b887b4569129db9c3eff320f9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fab83a15fc28ea1f04b7",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "3c30fe08a7edf948fa5a",
+    "url": "/static/js/2.a752cf04.chunk.js"
+  },
+  {
+    "revision": "fab83a15fc28ea1f04b7",
+    "url": "/static/js/main.18437dfd.chunk.js"
+  },
+  {
+    "revision": "cae4f9e8c631ccae32fb",
+    "url": "/static/js/runtime~main.4351b628.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
